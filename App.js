ed28e873fc2fe81f3b43cc3987aa53a72e16e20c
Aarav Urgaonkar <aaravurgaonkar123@gmail.com>
2023-05-20 23:59:14 -0700

    Update Real

--- App.js
+++ App.js
@@ -5,25 +5,33 @@ import InitialScreen from './screens/InitialScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import RegisterScreen from './screens/RegisterScreen';
 
+
 const Stack = createNativeStackNavigator();
 
+
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{headerShown: false}} name="Initial" component={InitialScreen} />
-        <Stack.Screen options={{headerShown: false}} name="Register" component={RegisterScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+ return (
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen
+       options={{headerShown: false}}
+       name="Initial"
+       component={InitialScreen}
+       />
+       <Stack.Screen
+       options={{headerShown: false}}
+       name="Register"
+       component={RegisterScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
 // test
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333333',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: '#333333',
+   alignItems: 'center',
+   justifyContent: 'center',
+ },
 });
-//abc
